e4dcd3ba8e246dd6c5f651fe07d76cfdcf9b67b5
AngeloAkuhwa <angeloakuhwa@gmail.com>
2025-09-23 23:17:00 +0100

    updated migrations

---
+++ Src/CTCare.Infrastructure/Migrations/20250923221439_AddLeaveCoreUpdate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTCare.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLeaveCoreUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "StartDate",
+                table: "LeaveRequests",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "LeaveRequests",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "StartDate",
+                table: "LeaveRequests",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "EndDate",
+                table: "LeaveRequests",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+    }
+}
